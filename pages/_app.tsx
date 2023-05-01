a04528a893c105fa62234971ffa952e14d36473c
estevamorais <demoraisestevam@gmail.com>
2023-05-01 14:06:47 -0300

    Create Edit User, Image Upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { Toaster } from 'react-hot-toast'
 import { SessionProvider } from 'next-auth/react'
 
 import Layout from '@/components/Layout'
+import EditModal from '@/components/modals/EditModal'
 import LoginModal from '@/components/modals/LoginModal'
 import RegisterModal from '@/components/modals/RegisterModal'
 
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal />
       <LoginModal />
       <RegisterModal />
       <Layout>
