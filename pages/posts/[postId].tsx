e61d8ae2e48e4f5ed9b308ce1389f97c90ad132d
estevamorais <demoraisestevam@gmail.com>
2023-05-04 16:27:20 -0300

    Create Comments

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -7,6 +7,7 @@ import usePost from '@/hooks/usePost'
 import Header from '@/components/Header'
 import PostItem from '@/components/Posts/PostItem'
 import Form from '@/components/Form'
+import CommentFeed from '@/components/Posts/CommentFeed'
 
 const PostView = () => {
   const router = useRouter()
@@ -31,6 +32,7 @@ const PostView = () => {
         isComment
         placeholder="Tweet your reply"
       />
+      <CommentFeed comments={fetchedPost?.comments} />
     </>
   )
 }
